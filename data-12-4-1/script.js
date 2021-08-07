70fe618aa24e0d6080bc0e0013fbfe4ca5a36449
Luis-Acevedo <arnieacevedo1996@gmail.com>
2021-08-07 18:51:33 -0400

    Completion of 12.4.1
    Completion of starter files and creating an event listener for the drop down menu

---
+++ data-12-4-1/script.js
@@ -0,0 +1,13 @@
+// Make sure to run "python -m http.server" in terminal in the same directory as files
+
+d3.selectAll("body").on("change", updatePage);
+
+function updatePage() {
+  var dropdownMenu = d3.selectAll("#selectOption").node();
+  var dropdownMenuID = dropdownMenu.id;
+  var selectedOption = dropdownMenu.value;
+
+  console.log(dropdownMenuID);
+  console.log(selectedOption);
+};
+
