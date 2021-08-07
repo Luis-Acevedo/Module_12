958defffd502a09fb8c81081bfb0151398d5175d
Luis-Acevedo <arnieacevedo1996@gmail.com>
2021-08-07 18:04:28 -0400

    Completion of 12.3.2
    Initialization of belly button info and function set up to call data

---
+++ data-12-3-2-resources/plot.js
@@ -0,0 +1,35 @@
+const url = "https://api.spacexdata.com/v2/launchpads";
+
+// Make sure to run "python -m http.server" in terminal in the same directory as files
+d3.json("samples.json").then(function(data){
+    console.log("hello");
+});
+
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person => person.wfreq);
+    console.log(wfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    console.log(wfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
